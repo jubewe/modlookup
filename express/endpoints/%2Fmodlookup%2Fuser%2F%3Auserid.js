c1d8265e660a00bd05a01b7c91cb54e927d4fe7a
jubewe <ju.be.we@posteo.de>
2023-02-24 17:41:08 +0100

    v0.1.8

--- express/endpoints/%2Fmodlookup%2Fuser%2F%3Auserid.js
+++
@@ -1,6 +0,0 @@
-const _rf = require("../../functions/_rf");
-
-/** @param {import("express").Request} req @param {import("express").Response} res @param {Boolean} ih */
-module.exports = (req, res, ih) => {
-    if(ih) return res.send(_rf("./html/modlookup/user.html"))
-};
